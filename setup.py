004c14c1628f8dde4997ef1949e8cd728d4c43c1
Francesco Milani <fmilani@bink.com>
2019-08-12 13:46:46 +0300

    init repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="shared-config-storage",
+    version="0.1",
+    author="Francesco Milani",
+    author_email="fmilani@bink.com",
+    description="shared configurations storage",
+    packages=["."],
+    url="https://git.bink.com/bink-platform/daedalus",
+    classifiers=("Programming Language :: Python :: 3",),
+)

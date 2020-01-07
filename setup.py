ca38f3c37bf18ffda41172dadaeda366c4bcdcd2
Francesco Milani <fmilani@bink.com>
2020-01-07 17:56:52 +0200

    add encryption lib and hopefully adapt setup correctly

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from setuptools import setup
 
 setup(
     name="shared_config_storage",
-    version="0.2",
+    version="1.0",
     author="Francesco Milani",
     author_email="fmilani@bink.com",
     description="shared configurations storage",
     url='https://git.bink.com/prototypes/shared-config-storage.git',
     packages=[
+        'shared_config_storage',
         'shared_config_storage/ubiquity'
     ],
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        'pycryptodome'
+    ],
     license="Internal",
     zip_safe=True,
     classifiers=("Programming Language :: Python :: 3",),

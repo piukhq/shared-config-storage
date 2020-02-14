5347a3769ed5b1aee9550db5ba4611de912f8f59
mmarsh <mmarsh@bink.com>
2020-02-14 17:59:15 +0000

    class added to secrets

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Library for configurations that needs to be shared between one or more projects
 - ubiquity payment card bin lookup
 - AES Crypto for membership cards' credentials
 
-- vault.secrets - read_vault to read secrets from vault
+- vault.secrets - read_vault to read secrets from vault 
+                - channel secrets can be loaded once on app start if VaultChannelSecrets is in global scope
--- shared_config_storage/vault/secrets.py
+++ shared_config_storage/vault/secrets.py
@@ -1,4 +1,64 @@
+import json
 import requests
+import logging
+
+
+class VaultReadError(Exception):
+    pass
+
+
+class VaultChannelSecrets:
+
+    _bundle_secrets = {}
+
+    def __init__(self, url, token, channel_path="/channels", vault_logger=None, vault_exception=VaultReadError):
+        """
+        On startup retrieves security credential values from channel secrets storage vault.
+        The returned record from read vault returns all data for every bundle from which jwt_secret is extracted
+
+        data is passed into _bundle_secrets which is used as a cache for API authentication by bundle
+
+        """
+        if vault_logger:
+            self.vault_logger = vault_logger
+        else:
+            self.vault_logger = logging.getLogger('channel_vault')
+
+        self.vault_exception = vault_exception
+        self.url = url
+        self.token = token
+        self.channel_path = channel_path
+
+    def load_secrets(self, local_channel_secrets=False, local_secrets_path="my_secrets.json"):
+
+        if local_channel_secrets:
+            self.vault_logger.info(f"Channel bundle secrets - from local file {local_secrets_path}")
+            with open(local_secrets_path) as fp:
+                self._bundle_secrets = json.load(fp)
+        else:
+            self.vault_logger.info(f"Channel bundle secrets - from vault at {self.url}  secrets: {self.channel_path}")
+            try:
+                self._bundle_secrets = read_vault(self.channel_path, self.url, self.token)
+            except requests.RequestException as e:
+                self.vault_logger.exception(f"Channel bundle secrets - Vault Exception {e}")
+                raise self.vault_exception(f'Channel bundle secrets - Exception {e}') from e
+
+        self.vault_logger.info(f"Channel bundle secrets - Found secrets for"
+                               f" {[bundle_id for bundle_id in self._bundle_secrets]}")
+
+    def get_jwt_secret(self, bundle_id):
+        try:
+            return self._bundle_secrets[bundle_id]['jwt_secret']
+        except KeyError as e:
+            self.vault_logger.error(f"No bundle defined: {e}")
+            raise self.vault_exception(f"JWT is invalid: {e}") from e
+
+    def get_pcard_secret(self, bundle_id):
+        try:
+            return self._bundle_secrets[bundle_id]['pcard']
+        except KeyError as e:
+            self.vault_logger.error(f"No pcard defined for bundle {bundle_id} exception {e}")
+            raise self.vault_exception(f"No pcard defined for bundle {bundle_id} exception {e}") from e
 
 
 class VaultError(Exception):

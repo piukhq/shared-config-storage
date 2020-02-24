939fce21ed74373864a224d5a4c43687e2c19827
kaziz <kaziz@bink.com>
2020-02-24 15:38:05 +0000

    Add enum for all credential types

--- shared_config_storage/credentials/utils.py
+++ shared_config_storage/credentials/utils.py
@@ -15,11 +15,15 @@ class AnswerTypeChoices(Enum):
     PAYMENT_CARD_ID = 4
 
 
-class EncryptedCredentials(Enum):
+class CredentialType(str, Enum):
+    USER_NAME = 'username'
+    CARD_NUMBER = 'card_number'
+    BARCODE = 'barcode'
     PASSWORD = 'password'
+    PLACE_OF_BIRTH = 'place_of_birth'
+    EMAIL = 'email'
     POSTCODE = 'postcode'
     MEMORABLE_DATE = 'memorable_date'
-    PLACE_OF_BIRTH = 'place_of_birth'
     PIN = 'pin'
     TITLE = 'title'
     FIRST_NAME = 'first_name'
@@ -36,3 +40,28 @@ class EncryptedCredentials(Enum):
     COUNTY = 'county'
     COUNTRY = 'country'
     REGULAR_RESTAURANT = 'regular_restaurant'
+    MERCHANT_IDENTIFIER = 'merchant_identifier'
+    PAYMENT_CARD_ID = 'payment_card_id'
+
+
+class EncryptedCredentials(Enum):
+    PASSWORD = CredentialType.PASSWORD.value
+    POSTCODE = CredentialType.POSTCODE.value
+    MEMORABLE_DATE = CredentialType.MEMORABLE_DATE.value
+    PLACE_OF_BIRTH = CredentialType.PLACE_OF_BIRTH.value
+    PIN = CredentialType.PIN.value
+    TITLE = CredentialType.TITLE.value
+    FIRST_NAME = CredentialType.FIRST_NAME.value
+    LAST_NAME = CredentialType.LAST_NAME.value
+    FAVOURITE_PLACE = CredentialType.FAVOURITE_PLACE.value
+    DATE_OF_BIRTH = CredentialType.DATE_OF_BIRTH.value
+    PHONE = CredentialType.PHONE.value
+    PHONE_2 = CredentialType.PHONE_2
+    GENDER = CredentialType.GENDER.value
+    ADDRESS_1 = CredentialType.ADDRESS_1.value
+    ADDRESS_2 = CredentialType.ADDRESS_2.value
+    ADDRESS_3 = CredentialType.ADDRESS_3.value
+    TOWN_CITY = CredentialType.TOWN_CITY.value
+    COUNTY = CredentialType.COUNTY.value
+    COUNTRY = CredentialType.COUNTRY.value
+    REGULAR_RESTAURANT = CredentialType.REGULAR_RESTAURANT.value

535657f500b7cdd02067cde523307107edf3ad96
Terry Cain <tcain@bink.com>
2020-06-29 17:01:52 +0000

    Updated CI to push to PyPi

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 import shared_config_storage
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="shared_config_storage",
     version=shared_config_storage.__version__,
     author="Francesco Milani",
     author_email="fmilani@bink.com",
     description="shared configurations storage",
+    long_description_content_type="text/markdown",
+    long_description=readme,
     url='https://git.bink.com/prototypes/shared-config-storage.git',
     packages=[
         'shared_config_storage',

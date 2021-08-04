f974394de043535db6d42965b44be2ecf22b1c83
Terry Cain <tcain@bink.com>
2021-08-04 15:54:48 +0000

    Ci

--- shared_config_storage/ubiquity/bin_lookup.py
+++ shared_config_storage/ubiquity/bin_lookup.py
@@ -1,26 +1,20 @@
 from collections import namedtuple
 
-BinMatch = namedtuple('BinMatch', 'type len value')
+BinMatch = namedtuple("BinMatch", "type len value")
 
 BIN_TO_PROVIDER = {
-    'visa': [
-        BinMatch(type='equal', len=1, value='4'),
+    "visa": [
+        BinMatch(type="equal", len=1, value="4"),
     ],
-    'amex': [
-        BinMatch(type='equal', len=2, value='34'),
-        BinMatch(type='equal', len=2, value='37')
-    ],
-    'mastercard': [
-        BinMatch(type='range', len=2, value=(51, 55)),
-        BinMatch(type='range', len=4, value=(2221, 2720))
-    ]
+    "amex": [BinMatch(type="equal", len=2, value="34"), BinMatch(type="equal", len=2, value="37")],
+    "mastercard": [BinMatch(type="range", len=2, value=(51, 55)), BinMatch(type="range", len=4, value=(2221, 2720))],
 }
 
 
 def bin_to_provider(bin_first_six: str) -> str:
     match_to_first_6 = {
-        'range': lambda match: match.value[0] <= int(bin_first_six[:match.len]) <= match.value[1],
-        'equal': lambda match: bin_first_six[:match.len] == match.value
+        "range": lambda match: match.value[0] <= int(bin_first_six[: match.len]) <= match.value[1],
+        "equal": lambda match: bin_first_six[: match.len] == match.value,
     }
 
     for provider, values in BIN_TO_PROVIDER.items():
@@ -28,4 +22,4 @@ def bin_to_provider(bin_first_six: str) -> str:
             if match_to_first_6[bin_match.type](bin_match):
                 return provider
 
-    return 'other'
+    return "other"
---
+++ tests/test_vault_secrets.py
@@ -0,0 +1,24 @@
+import pytest
+
+from shared_config_storage.vault import secrets
+
+
+def test_read_vault_bad_status(vault):
+    vault.add_secret("data", json_data={}, status=404)
+
+    with pytest.raises(secrets.VaultError):
+        secrets.read_vault("/data", vault.url, "sometoken")
+
+
+def test_read_vault_timeout():
+
+    with pytest.raises(secrets.VaultError):
+        secrets.read_vault("/data", "http://0.0.0.0/", "sometoken")
+
+
+def test_read_vault(vault):
+    secret_data = {"blah": 1}
+    vault.add_secret("data", json_data={"data": secret_data}, status=200)
+
+    secret = secrets.read_vault("/data", vault.url, "sometoken")
+    assert secret == secret_data
